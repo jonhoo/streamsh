05f8863a45a947dcd6525f8f4ebe69f4f6bb7fed
Jon Gjengset <jon@thesquareplanet.com>
2015-08-30 18:25:24 -0400

    Handle encrypted file arguments in jwplayer.
    This is pretty neat. Many sites (vidbull.com is among them) have started
    encrypting their file handles so that you can't read the URL to the
    video file straight from the (obscured and encoded) JS source. However,
    since the client eventually has to request the file, the key is stored
    in the SWF. After some digging, it turns out that the key is the
    `asproject.swf` plugin that is loaded. Some digging with
    http://www.showmycode.com/ revealed that this file loads a file called
    `obc.swf` from the same directory, which in turn does encryption and
    decryption using the AS3 crypto library As3Crypto. The key is hard-coded
    into the SWF in five parts (see `h1` through `h5` in `decryptor.as` in
    the disassembly of `obc.swf`). Using `swfdump` from the `swftools`
    package easily gets us this key, which can then be used to decrypt the
    video's URL using openssl (after hex-decoding the file argument).

--- tricks/jwplayer.sh
+++ tricks/jwplayer.sh
@@ -6,6 +6,39 @@ try_jwplayer() {
 		return
 	fi
 
+	m=$(echo "$url" | grep :)
+	if [[ -z "$m" ]]; then
+		echo "Decoding '$url'" > /dev/stderr
+
+		# get obc.swf which contains the key
+		obc=$(echo "$1" | sed 's/.*\(http[^"]*asproject.swf\).*/\1/' | sed 's/asproject.swf/obc.swf/')
+		tmp=$(mktemp obc.swf.XXXXXX)
+		curl -s "$obc" > "$tmp"
+
+		# extract key
+		key=$(swfdump -D "$tmp" 2>/dev/null | grep -A4 'decryptor::h1' | grep String | sed 's/.*= //' | paste -sd' ' | tr -d ' ')
+		rm "$tmp"
+		echo "$key" > .cache/prev-key
+
+		# get binary data
+		(echo -n "$url" | xxd -r -p; echo "") > .cache/prev-enc
+
+		# decode
+		dec=$(
+			(echo -n "$url" | xxd -r -p; echo "") \
+			| base64 --wrap=0 \
+			| openssl enc -d -A -base64 -aes-128-ecb -K "$key" \
+			2>/dev/null
+		)
+		m=$(echo "$dec" | grep :)
+		if [[ -z "$m" ]]; then
+			echo "Damn..." > /dev/stderr
+			return
+		fi
+		url="$dec"
+		echo "Decoded to $dec" > /dev/stderr
+	fi
+
 	title="unknown-series-$$"
 	owned="true"
 }
